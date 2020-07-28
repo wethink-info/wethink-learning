8ef849a44dcab8c8320147f5725d788bee333c72
maryem123-ops <maryembouchiba2018@gmail.com>
2020-07-28 13:34:08 +0100

    fix error index page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
 // styles for this kit
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss?v=1.4.0";
@@ -23,7 +22,6 @@ ReactDOM.render(
       <Switch>
         <Route path="/index" render={(props) => <Index {...props} />} />
         <Route
-          
           path="/nucleo-icons"
           render={(props) => <NucleoIcons {...props} />}
         />
